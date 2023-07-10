dbed6a1d69cd1b6f9ffc9f062a0155ff1910ffd5
guynachshon <guy.na8@gmail.com>
2023-07-10 18:33:25 +0300

    feat: gh action, remove rubygems

--- test/smoke/cypress/plugins/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (on, config) => {
-  config.env.nexusPass = process.env.NEXUS_PASS;
-  config.env.checkmarxToken = process.env.TEST_checkmarx_TOKEN;
-  config.env.checkmarxOrg = process.env.TEST_checkmarx_ORG;
-  return config;
-}
